dca1d24377340f993526c53422034c9a7cde49cf
Robsutar <robsonsutar@gmail.com>
2024-11-10 18:52:30 -0300

    feat: add ICustomRecipesManager

--- src/main/java/to/lodestone/observerapi/IObserverAPI.java
+++ src/main/java/to/lodestone/observerapi/IObserverAPI.java
@@ -3,8 +3,11 @@ package to.lodestone.observerapi;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import to.lodestone.observerapi.api.data.GameState;
+import to.lodestone.observerapi.api.manager.ICustomRecipesManager;
 
 public interface IObserverAPI {
+
+    ICustomRecipesManager getCustomRecipesManager();
 
     GameState getGameState();
     void setGameState(Player player, GameState gameState);
