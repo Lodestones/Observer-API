f0bf9ffe64b47f295ac046936d992ec4e162372c
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-03-25 00:29:57 -0700

    feat: setters

--- src/main/java/to/lodestone/observerapi/api/manager/IMechanicsManager.java
+++ src/main/java/to/lodestone/observerapi/api/manager/IMechanicsManager.java
@@ -25,15 +25,59 @@ public interface IMechanicsManager {
     boolean canTradeWithVillagers();
     boolean canUseRespawnAnchors();
     boolean canBedBomb();
+
     boolean canPortalTrap();
+
     boolean canIndirectPVP();
+
     boolean shouldGlobalBlockDecay();
+
     boolean isUnbreakableSpawners();
+
     boolean isBetterWorldBorder();
+
     boolean shouldMatchWorldBorders();
+
     boolean canBrewPotions();
+
     ICombatLoggingManager getCombatLoggingManager();
+
     boolean isNetherAllowed();
+
     boolean isEndAllowed();
+
+    void setSleepingAllowed(boolean sleepingAllowed);
+
+    void setEnchantItems(boolean enchantItems);
+
+    void setUseAnvils(boolean useAnvils);
+
+    void setUseEnderChests(boolean useEnderChests);
+
+    void setBreedVillagers(boolean breedVillagers);
+
+    void setTradeWithVillagers(boolean tradeWithVillagers);
+
+    void setUseRespawnAnchors(boolean useRespawnAnchors);
+
+    void setBedBomb(boolean bedBomb);
+
+    void setPortalTrap(boolean portalTrap);
+
+    void setIndirectPVP(boolean indirectPVP);
+
+    void setGlobalBlockDecay(boolean globalBlockDecay);
+
+    void setUnbreakableSpawners(boolean unbreakableSpawners);
+
+    void setBetterWorldBorder(boolean betterWorldBorder);
+
+    void setMatchWorldBorders(boolean matchWorldBorders);
+
+    void setBrewPotions(boolean brewPotions);
+
+    void setNetherAllowed(boolean netherAllowed);
+
+    void setEndAllowed(boolean endAllowed);
 
 }
