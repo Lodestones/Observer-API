5935887c91c5074e9944bbe325651a6dda03b275
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-05-26 14:55:00 -0700

    feat(api): add WorldEvent.java

--- src/main/java/gg/lode/observerapi/api/data/WorldEvent.java
+++ src/main/java/gg/lode/observerapi/api/data/WorldEvent.java
@@ -1,12 +1,21 @@
 package gg.lode.observerapi.api.data;
 
+import org.bukkit.Material;
+
 public class WorldEvent {
+
+    private Material itemIcon;
     private String id;
     private String translationId;
 
-    public WorldEvent(String id, String translationId) {
+    public WorldEvent(Material itemIcon, String id, String translationId) {
         this.id = id;
+        this.itemIcon = itemIcon;
         this.translationId = translationId;
+    }
+
+    public Material getItemIcon() {
+        return itemIcon;
     }
 
     public String getId() {
@@ -15,6 +24,10 @@ public class WorldEvent {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public void setItemIcon(Material itemIcon) {
+        this.itemIcon = itemIcon;
     }
 
     public String getTranslationId() {
