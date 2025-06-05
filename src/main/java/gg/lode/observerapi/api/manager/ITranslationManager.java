aec9e1343ee12fa0a9249490576d5099cbb1dfb0
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-06-05 03:28:35 -0700

    feat(core): support variablecontext for translation manager

--- src/main/java/gg/lode/observerapi/api/manager/ITranslationManager.java
+++ src/main/java/gg/lode/observerapi/api/manager/ITranslationManager.java
@@ -1,7 +1,10 @@
 package gg.lode.observerapi.api.manager;
 
+import gg.lode.bookshelfapi.api.util.VariableContext;
 import gg.lode.observerapi.api.data.Translation;
 
 public interface ITranslationManager {
     Translation translate(String path);
+
+    Translation translate(String path, VariableContext context);
 }
