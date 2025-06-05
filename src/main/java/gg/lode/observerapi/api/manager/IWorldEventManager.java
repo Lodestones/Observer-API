6996e1587f249566ca5320ca0172379d37021ab3
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-06-05 02:08:11 -0700

    feat(api): more world event utility

--- src/main/java/gg/lode/observerapi/api/manager/IWorldEventManager.java
+++ src/main/java/gg/lode/observerapi/api/manager/IWorldEventManager.java
@@ -1,14 +1,25 @@
 package gg.lode.observerapi.api.manager;
 
 import gg.lode.observerapi.api.data.WorldEvent;
+
 import java.util.List;
 
 public interface IWorldEventManager {
     List<WorldEvent> getWorldEvents();
+
     WorldEvent getWorldEvent(String id);
+
     void addWorldEvent(WorldEvent event);
+
     void removeWorldEvent(String id);
+
     void updateWorldEvent(WorldEvent event);
+
     void setCurrentWorldEvent(WorldEvent event);
+
+    boolean hasCurrentWorldEvent();
+
+    boolean isWorldEventActive(String id);
+
     WorldEvent getCurrentWorldEvent();
 } 
