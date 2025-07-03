85c1cfe2ba03be9439c2412051aed888b1b5775c
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-07-03 02:20:53 -0700

    feat(api+core): boilerplate for adding command list for worldevents

--- src/main/java/gg/lode/observerapi/api/data/WorldEvent.java
+++ src/main/java/gg/lode/observerapi/api/data/WorldEvent.java
@@ -2,16 +2,28 @@ package gg.lode.observerapi.api.data;
 
 import org.bukkit.Material;
 
+import java.util.List;
+
 public class WorldEvent {
 
     private Material itemIcon;
     private String id;
     private String translationId;
+    private List<String> commands;
 
-    public WorldEvent(Material itemIcon, String id, String translationId) {
+    public WorldEvent(Material itemIcon, String id, String translationId, List<String> commands) {
         this.id = id;
         this.itemIcon = itemIcon;
         this.translationId = translationId;
+        this.commands = commands;
+    }
+
+    public List<String> getCommands() {
+        return commands;
+    }
+
+    public void setCommands(List<String> commands) {
+        this.commands = commands;
     }
 
     public Material getItemIcon() {
