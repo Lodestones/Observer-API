cfb5dee901cdfaab9bada83a1e71f5bc203559a2
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-08-20 17:10:58 -0700

    feat(api): add IHelpOpManager.java

---
+++ src/main/java/gg/lode/observerapi/api/manager/IHelpOpManager.java
@@ -0,0 +1,13 @@
+package gg.lode.observerapi.api.manager;
+
+public interface IHelpOpManager {
+
+    boolean isEnabled();
+
+    void setEnabled(boolean enabled);
+
+    long getCooldown();
+
+    void setCooldown(long milliseconds);
+
+}
